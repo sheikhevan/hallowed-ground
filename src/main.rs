b1b7c644b4c795f976a4ae6d5f23d6f810a6c842
Evan Alvarez <evanfalvarez@tutamail.com>
2025-10-03 21:18:29 -0500

    you can scroll now

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::{input::mouse::MouseWheel, prelude::*, window::PrimaryWindow};
 
 #[derive(Component)]
 struct Camera {
     speed: f32,
     margin: f32,
+
+    zoom_speed: f32,
+    max_zoom: f32,
+    min_zoom: f32,
 }
 
 impl Default for Camera {
@@ -11,6 +15,10 @@ impl Default for Camera {
         Self {
             speed: 300.0,
             margin: 35.0,
+
+            zoom_speed: 0.1,
+            max_zoom: 3.0,
+            min_zoom: 0.5,
         }
     }
 }
@@ -18,8 +26,8 @@ impl Default for Camera {
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup_camera)
-        .add_systems(Update, camera_edge_scroll)
+        .add_systems(Startup, (setup_camera, temp_setup_shapes))
+        .add_systems(Update, (camera_edge_scroll, camera_zoom))
         .run();
 }
 
@@ -51,12 +59,12 @@ fn camera_edge_scroll(
     if cursor_pos.x > win_width - camera.margin {
         move_dir.x += 1.0;
     }
-    // Check bottom edge
-    if cursor_pos.y < camera.margin {
-        move_dir.y -= 1.0;
-    }
     // Check top edge
     if cursor_pos.y > win_height - camera.margin {
+        move_dir.y -= 1.0;
+    }
+    // Check bottom edge
+    if cursor_pos.y < camera.margin {
         move_dir.y += 1.0;
     }
 
@@ -68,6 +76,77 @@ fn camera_edge_scroll(
     }
 }
 
+fn camera_zoom(
+    mut msg_scroll: MessageReader<MouseWheel>,
+    mut q_camera: Query<(&mut Transform, &Camera)>,
+) {
+    let Ok((mut camera_transform, camera)) = q_camera.single_mut() else {
+        return;
+    };
+
+    for msg in msg_scroll.read() {
+        // Gets the scroll amount (+ is zoom in, - is zoom out)
+        let zoom_delta = msg.y * camera.zoom_speed;
+
+        // Calculate new scale
+        let current_scale = camera_transform.scale.x;
+        let new_scale = (current_scale - zoom_delta).clamp(camera.min_zoom, camera.max_zoom);
+
+        camera_transform.scale = Vec3::splat(new_scale);
+    }
+}
+
 fn setup_camera(mut commands: Commands) {
     commands.spawn((Camera2d, Camera::default()));
+}
+
+const X_EXTENT: f32 = 900.;
+
+fn temp_setup_shapes(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let shapes = [
+        meshes.add(Circle::new(50.0)),
+        meshes.add(CircularSector::new(50.0, 1.0)),
+        meshes.add(CircularSegment::new(50.0, 1.25)),
+        meshes.add(Ellipse::new(25.0, 50.0)),
+        meshes.add(Annulus::new(25.0, 50.0)),
+        meshes.add(Capsule2d::new(25.0, 50.0)),
+        meshes.add(Rhombus::new(75.0, 100.0)),
+        meshes.add(Rectangle::new(50.0, 100.0)),
+        meshes.add(RegularPolygon::new(50.0, 6)),
+        meshes.add(Triangle2d::new(
+            Vec2::Y * 50.0,
+            Vec2::new(-50.0, -50.0),
+            Vec2::new(50.0, -50.0),
+        )),
+        meshes.add(Segment2d::new(
+            Vec2::new(-50.0, 50.0),
+            Vec2::new(50.0, -50.0),
+        )),
+        meshes.add(Polyline2d::new(vec![
+            Vec2::new(-50.0, 50.0),
+            Vec2::new(0.0, -50.0),
+            Vec2::new(50.0, 50.0),
+        ])),
+    ];
+    let num_shapes = shapes.len();
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        // Distribute colors evenly across the rainbow.
+        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+
+        commands.spawn((
+            Mesh2d(shape),
+            MeshMaterial2d(materials.add(color)),
+            Transform::from_xyz(
+                // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
+                -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
+                0.0,
+                0.0,
+            ),
+        ));
+    }
 }
